f4bb3b9d96e19b14026862682367a6b0beb00cdf
egarcia147 <81089528+egarcia147@users.noreply.github.com>
2021-04-24 09:27:40 -0600

    Add files via upload

---
+++ menu.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage; 
+
+/**
+ *
+ * @author Fernando Garcia
+ */
+public class menu extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+        } catch() {}
+    } 
+    
+    
+}
